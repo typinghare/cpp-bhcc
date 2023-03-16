7fb4b5aae5f7729cdc0a856b4189d1775f6d715e
James Chan <jameschan312.cn@gmail.com>
2023-03-16 16:10:01 -0400

    March 16, 2023

--- src/assignment/4/Assignment4.cpp
+++ src/assignment/4/Assignment4.cpp
@@ -32,16 +32,16 @@ struct Account {
     string ownerName;       // the name of the owen of this account
     char accountType;       // S = savings, C = checking
     double rate;            // (annual) interest rate
-    double balance;         // unit: dollar
+    double balance;         // balance (in American dollar)
 };
 
-double deposit(Account *account, double money);
+double deposit(Account &account, double money);
 
-double withdraw(Account *account, double money);
+double withdraw(Account &account, double money);
 
-void updateAccountInterest(Account *account, int numMonths);
+void updateAccountInterest(Account &account, int numMonths);
 
-void printAccount(Account *account, ostream &os);
+void printAccount(Account &account, ostream &os);
 
 int randBetween(int lowerBound, int upperBound);
 
@@ -50,6 +50,7 @@ string generateAccountNumber();
 int main() {
     srand(time(nullptr));
 
+    // @ PART 1
     // create a savings account and checking account for Selena Gomes
     Account savingsAccount = Account();
     savingsAccount.accountType = 'S';
@@ -61,25 +62,31 @@ int main() {
     checkingAccount.balance = 500;
     checkingAccount.rate = 0.0;
 
+    // test update account interest
+    cout << "The balance in the savings account: " << savingsAccount.balance << endl;
+    updateAccountInterest(savingsAccount, 24);
+    cout << "The balance in the savings account: " << savingsAccount.balance << endl << endl;
+
+    // @ PART 2
     // create an array of three savings accounts
     const int NUM_ACCOUNT = 3;
     const double ACCOUNT_BALANCE_MIN = 75.0;
     const double ACCOUNT_BALANCE_MAX = 200.0;
     const double INTEREST_RATE = 0.004; // 4%
-    Account **accountArray = new Account *[NUM_ACCOUNT];
+    Account *accountArray = new Account[NUM_ACCOUNT];
 
     for (int i = 0; i < NUM_ACCOUNT; ++i) {
-        Account *account = new Account();
-        account->accountNumber = "";
-        account->accountType = 'S';
-        account->rate = INTEREST_RATE;
-        account->balance = randBetween(ACCOUNT_BALANCE_MIN, ACCOUNT_BALANCE_MAX);
+        Account account = Account();
+        account.accountNumber = generateAccountNumber();
+        account.accountType = 'S';
+        account.rate = INTEREST_RATE;
+        account.balance = randBetween(ACCOUNT_BALANCE_MIN, ACCOUNT_BALANCE_MAX);
         accountArray[i] = account;
     }
 
-    accountArray[0]->ownerName = "Jackson";
-    accountArray[1]->ownerName = "Emma";
-    accountArray[2]->ownerName = "Noah";
+    accountArray[0].ownerName = "Jackson";
+    accountArray[1].ownerName = "Emma";
+    accountArray[2].ownerName = "Noah";
 
     // print all the savings accounts
     cout << "Before deposit and withdrawal:" << endl;
@@ -104,7 +111,6 @@ int main() {
     }
 
     // release
-    for (int i = 0; i < NUM_ACCOUNT; ++i) delete[] accountArray[i];
     delete[] accountArray;
 }
 
@@ -114,14 +120,14 @@ int main() {
  * @param money the money to deposit
  * @return the balance of the account after deposit; -1 if the money given is a non-positive number.
  */
-double deposit(Account *account, double money) {
+double deposit(Account &account, double money) {
     if (money <= 0) {
         cout << "The money to deposit should be a positive number." << endl;
         return -1;
     }
 
-    account->balance += money;
-    return account->balance;
+    account.balance += money;
+    return account.balance;
 }
 
 /**
@@ -131,11 +137,11 @@ double deposit(Account *account, double money) {
  * @return the balance of the account after withdrawal; -1 if the money to withdraw is
  * greater than the balance.
  */
-double withdraw(Account *account, double money) {
-    if (money > account->balance) return -1;
+double withdraw(Account &account, double money) {
+    if (money > account.balance) return -1;
 
-    account->balance -= money;
-    return account->balance;
+    account.balance -= money;
+    return account.balance;
 }
 
 /**
@@ -143,25 +149,26 @@ double withdraw(Account *account, double money) {
  * @param account the account to update interest
  * @param numMonths the number of months
  */
-void updateAccountInterest(Account *account, int numMonths) {
-    double interest = (account->balance * numMonths * account->rate) / 12000;
+void updateAccountInterest(Account &account, int numMonths) {
+    double interest = (account.balance * numMonths * account.rate) / 12;
+    cout << interest << endl;
 
-    account->balance += interest;
+    account.balance += interest;
 }
 
 /**
  * Print the information of an account.
  * @param account account to print
  */
-void printAccount(Account *account, ostream &os) {
+void printAccount(Account &account, ostream &os) {
     // we assume that the type of the account it either 'S' or 'C'
-    string accountType = account->accountType == 'S' ? "Savings Account" : "Checking Account";
+    string accountType = account.accountType == 'S' ? "Savings Account" : "Checking Account";
 
-    os << "Account number: " << account->accountNumber << endl
-       << "Owner name: " << account->ownerName << endl
+    os << "Account number: " << account.accountNumber << endl
+       << "Owner name: " << account.ownerName << endl
        << "Account type: " << accountType << endl
-       << "Interest rate: " << account->rate << endl
-       << "Balance: " << account->balance << endl;
+       << "Interest rate: " << account.rate << endl
+       << "Balance: " << account.balance << endl;
 }
 
 /**
@@ -172,4 +179,18 @@ void printAccount(Account *account, ostream &os) {
  */
 int randBetween(int lowerBound, int upperBound) {
     return rand() % (upperBound - lowerBound + 1) + lowerBound;
+}
+
+/**
+ * Generates an account number. An account number should have six digits.
+ * @return an account number.
+ */
+string generateAccountNumber() {
+    static const int ACCOUNT_NUMBER_LENGTH = 6;
+    static int number = 0;
+
+    string accountNumber = to_string(++number);
+    int len = accountNumber.length();
+
+    return string(ACCOUNT_NUMBER_LENGTH - len, '0') + accountNumber;
 }
