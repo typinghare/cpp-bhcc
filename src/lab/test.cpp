689c56515548c4ea18c55403925f889932f0c8c0
James Chan <jameschan312.cn@gmail.com>
2023-03-23 11:45:03 -0400

    March 23, 2023

--- src/lab/test.cpp
+++ src/lab/test.cpp
@@ -2,11 +2,41 @@
 
 using namespace std;
 
-class MyClass {
-public:
-    int num = 5;
-
-    void setNum(int _num) {
-        num = _num;
-    }
+struct Time {
+    int hour;
+    int minute;
+    int second;
 };
+
+struct Date {
+    int year;
+    int month;
+    int day;
+};
+
+struct DateTime {
+    Date *date;
+    Time *time;
+};
+
+int main() {
+    Time time;
+    Date date;
+    DateTime dateTime;
+
+    time.hour = 10;
+    time.minute = 10;
+    time.second = 10;
+
+    date.year = 2023;
+    date.month = 3;
+    date.day = 15;
+
+    dateTime.date = &date;
+    dateTime.time = &time;
+
+    dateTime.time->hour = 12;
+    cout << time.hour << endl;
+
+    return 0;
+}
